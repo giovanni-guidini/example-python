4df467ebf4bd1b85db46bef5aba16d9a161043cd
Gguidini <giovanni@codecov.io>
2024-05-31 13:35:14 +0200

    add more paths to the coverage report output

--- tests/test_awesome.py
+++ tests/test_awesome.py
@@ -1,6 +1,11 @@
 from app.inner.app.inner.awesome import g
+from app.inner.inner import f
 
 
 def test_g():
     assert g(2) == 4
     assert g(1000) == 2000
+
+
+def test_f():
+    assert f(10) == 1010
