d9020f8774ff2f6de54af864c9d3d09bf068cae7
Gguidini <giovanni@codecov.io>
2024-06-27 21:35:36 +0200

    no coverage drop

--- app/calculator.py
+++ app/calculator.py
@@ -11,6 +11,10 @@ class Calculator:
     def divide(x, y):
         if y == 0:
             return "Cannot divide by 0"
+        print("These lines will not cause coverage drop")
+        print("These lines will not cause coverage drop")
+        print("These lines will not cause coverage drop")
+        print("These lines will not cause coverage drop")
         return x * 1.0 / y
 
     def sqrt(x, precision=0.0005):
